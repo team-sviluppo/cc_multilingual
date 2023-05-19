ef84990f8bc4df03f009e497b6908c5a0a266b87
Cristiano Revil <49713716+cristianorevil@users.noreply.github.com>
2023-05-19 16:14:38 +0200

    renaming repository

--- multilingual.py
+++ multilingual.py
@@ -6,9 +6,7 @@ from cat.utils import log
 
 
 def LoadConfiguration():
-    with open(
-        "./cat/plugins/cheshire-cat-multilingual-libretranslate/settings.json"
-    ) as json_file:
+    with open("./cat/plugins/cc_multilingual/settings.json") as json_file:
         return json.load(json_file)
 
 
@@ -17,7 +15,7 @@ mutlilangual_conf = LoadConfiguration()
 
 def translate_text(inputText, source, target):
     if "libretranslate_url" in mutlilangual_conf:
-        url = mutlilangual_conf["libretranslate_url"]
+        url = mutlilangual_conf["libretranslate_url"] + "/translate"
         payload = {
             "q": inputText,
             "source": source,
@@ -36,11 +34,31 @@ def translate_text(inputText, source, target):
         return inputText
 
 
+def detect_language(inputText):
+    if mutlilangual_conf["lang"] == "auto":
+        url = mutlilangual_conf["libretranslate_url"] + "/detect"
+        payload = {
+            "q": inputText,
+            "api_key": mutlilangual_conf["api_key"],
+        }
+        headers = {"Content-Type": "application/json"}
+        response = requests.post(url, data=json.dumps(payload), headers=headers)
+        log("Language detected: " + response.text)
+        detected = response.json()
+        return detected[0]["language"]
+    else:
+        return mutlilangual_conf["lang"]
+
+
 @hook(priority=1)
 def before_cat_sends_message(message, cat):
     source = "en"
     if "libretranslate_url" in mutlilangual_conf:
-        target = mutlilangual_conf["mylang"]
+        key = "source_lang"
+        if key in cat.working_memory:
+            target = cat.working_memory["source_lang"]
+        else:
+            target = "en"
     else:
         target = "en"
     log("Starting translation: " + message["content"])
@@ -54,11 +72,12 @@ def before_cat_sends_message(message, cat):
 def before_cat_reads_message(user_message_json, cat):
     target = "en"
     if "libretranslate_url" in mutlilangual_conf:
-        source = mutlilangual_conf["mylang"]
+        source = detect_language(user_message_json["text"])
     else:
         source = "en"
-    log("Starting translation: " + user_message_json["text"])
+    log("Starting translation: " + user_message_json["text"] + " - lang: " + source)
     translated_message = translate_text(user_message_json["text"], source, target)
-    log("End translation:" + translated_message)
+    log("End translation:" + translated_message + " - lang: " + target)
     user_message_json["text"] = translated_message
+    cat.working_memory["source_lang"] = source
     return user_message_json
